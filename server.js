c59d1279380716b7532fe3541c658aaa93f5916a
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-23 13:40:05 -0600

    defined all routes in server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get("/", (req, res) => res.send("API Running"));
 
 // define routes
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
