88fa2d7918629dea0b5b1bc11717409829e9d853
cosmicChimp <ian.t.marrs@gmail.com>
2020-06-23 13:38:01 -0600

    added routes and updated server.js to define routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 
+const connectDB = require("./config/db");
+
 const app = express();
 
+// connect to database
+
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
