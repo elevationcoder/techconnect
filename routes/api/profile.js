ba836eab0ce76d4810bd100ecc49e72ac5a9f025
cosmicChimp <ian.t.marrs@gmail.com>
2020-07-23 15:12:47 -0600

    added ability to get user repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 // @route GET api/profile/me
 // @desc Get current users profile
@@ -285,9 +287,40 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     const removeIndex = profile.education
       .map((item) => item.id)
       .indexOf(req.params.edu_id);
+
     profile.education.splice(removeIndex, 1);
+
     await profile.save();
+
     res.json(profile);
+  } catch (err) {
+    console.error(err.messages);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repos from github
+// @ access Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubsecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.messages);
     res.status(500).send("Server Error");
