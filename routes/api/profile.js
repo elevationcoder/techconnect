3c4ed782c50f5483efa2e890f304f777792ea53e
cosmicChimp <ian.t.marrs@gmail.com>
2020-07-22 18:29:30 -0600

    added delete profile and user capability to profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,4 +132,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user and posts
+// @ access Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
